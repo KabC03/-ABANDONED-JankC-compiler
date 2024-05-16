d038cdb0a52209b9096c949c24bae7b410f1f0e7
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-17 02:39:16 +1000

    Create types.c

---
+++ types.c
@@ -0,0 +1,15 @@
+/*
+ *  List of valid tokens
+ *
+*/
+char* validTokens[] = {
+    "void", "int", "float", "char", "long", "unsigned",
+    "if", "elif", "else",
+    "for", "while",
+    "none", "VAL_MIN", "VAL_MAX",
+    "+", "-", "*", "/", "%%", "=>", ">", "<=", "<", "==", "!=", "=",
+    "call", "return",
+    "read", "output", "allocate", "free", "rand", "sleep",
+    "[", "]", "{", "}", "(", ")", ",", ";", "@", "$",
+    "/*", "#"
+}; 
