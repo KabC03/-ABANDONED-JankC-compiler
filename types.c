b97a0d003ee18f97456c8efed3cd623291e3b3bb
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-17 03:15:11 +1000

    Update types.c

--- types.c
+++ types.c
@@ -1,15 +1,24 @@
 /*
- *  List of valid tokens
+ * valid_tokens.c
  *
-*/
+ * Description:
+ * This file contains an array of strings representing the valid syntax tokens that can be recognized by the compiler's lexer.
+ * Each token corresponds to a syntactic element used in the language, including data types, control structures, operators, 
+ * punctuation, and other special symbols necessary for the parsing process.
+ *
+ * Usage:
+ * This array is utilized during the lexical analysis phase to match sequences of characters in the source code to known tokens,
+ * facilitating the tokenization process which precedes syntax analysis and AST construction.
+ */
+
 char* validTokens[] = {
-    "void", "int", "float", "char", "long", "unsigned",
-    "if", "elif", "else",
-    "for", "while",
-    "none", "VAL_MIN", "VAL_MAX",
-    "+", "-", "*", "/", "%%", "=>", ">", "<=", "<", "==", "!=", "=",
-    "call", "return",
-    "read", "output", "allocate", "free", "rand", "sleep",
-    "[", "]", "{", "}", "(", ")", ",", ";", "@", "$",
-    "/*", "#"
-}; 
+    "void", "int", "float", "char", "long", "unsigned",  // Data types
+    "if", "elif", "else",                              // Conditional statements
+    "for", "while",                                    // Loop structures
+    "none", "VAL_MIN", "VAL_MAX",                      // Constants
+    "+", "-", "*", "/", "%", "=>", ">", "<=", "<", "==", "!=", "=",  // Operators
+    "call", "return",                                  // Function calls and returns
+    "read", "output", "allocate", "free", "rand", "sleep", // Built-in functions
+    "[", "]", "{", "}", "(", ")", ",", ";", "@", "$",  // Punctuation and special symbols
+    "/*", "#"                                          // Comment delimiters
+};
