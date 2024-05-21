18fc876722666820df59d9a3791859f11997acc9
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-21 20:42:04 +1000

    Update intepret_IR.h

--- src/intepret_IR.h
+++ src/intepret_IR.h
@@ -83,7 +83,7 @@ VM Notes:
     - Print prints a single character to the interpreter terminal.
     - The VM does not store anything other than function addresses on the stack. All function calls receive arguments by reference.
     - The stack starts at the end of the allocated heap (e.g if memory is 64 bytes, then stack starts at byte 64 and grows backwards)
-    - Program counter indexes BYTES not bits
+    - Program counter indexes BITS not BYTES
     - Instruction size is 4x sizeof(long long int)
         - 1: instruction
         - 2: R1
