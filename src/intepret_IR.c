5a35af1c8199896ae0571a24682987bd063f436f
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-21 20:52:38 +1000

    Update intepret_IR.c

--- src/intepret_IR.c
+++ src/intepret_IR.c
@@ -1,18 +1,31 @@
 #include "intepret_IR.h"
 
 
-typedef struct VirtualMachine {
-
-    //Note void pointers used - this is because memory can hold any datatype
-    void *registerArray;
-    void *ramArray;
-
-    //Indexes BITS
-    size_t programCounter;
-
-} VirtualMachine;
+#define BYTE 1
 
 
+bool initialise_virtual_machine(VirtualMachine *VMout, size_t RAMsize, size_t numRegisters) {
+
+    VMout->numRegisters = numRegisters;
+    VMout->RAMsize = RAMsize;
+    VMout->programCounter = 0;
+
+    VMout->registerArray = (void*)malloc(BYTE * numRegisters); 
+    VMout->ramArray = (void*)malloc(BYTE * RAMsize);
+
+    if(VMout->registerArray == NULL || VMout->ramArray == NULL) {
+        if(VMout->ramArray == NULL) {
+            free(VMout->registerArray);
+        } else if(VMout->registerArray == NULL){
+            free(VMout->ramArray);
+        } else {
+            //Should never happen but left here for readability
+        }
+    }
+
+
+    return true;
+}
 
 
 
