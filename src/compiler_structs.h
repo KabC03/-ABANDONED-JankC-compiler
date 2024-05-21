265d7572960cb389c8afcca34e446fcc3f72f825
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-21 18:01:27 +1000

    Update compiler_structs.h

--- src/compiler_structs.h
+++ src/compiler_structs.h
@@ -55,10 +55,7 @@ typedef enum TOKEN_TYPE {
     TOK_OPERATOR, // '+', '-', etc (INCLUDES FUNCTIONS)
     TOK_CURVED_BRACE, // '(' and ')' ONLY
     TOK_COMPARATOR, //'!=', '==', '>=', etc
-
-
-
-    //Datatypes
+        //Datatypes
     TOK_VOID,
     TOK_INT,
     TOK_FLOAT,
