1ad72a4d584aece3924a0d16ba57bdb9994ccd44
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-21 17:14:24 +1000

    Create tokeniser.h

---
+++ src/tokeniser.h
@@ -0,0 +1,19 @@
+
+/*
+ * tokeniser.h
+ *
+ */
+#ifndef TOKENISER_H 
+#define TOKENISER_H 
+#include "compiler_structs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+Token *tokenise(char *inputLine);
+
+#endif 
+
+
+
