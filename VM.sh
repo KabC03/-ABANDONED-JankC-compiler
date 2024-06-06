dd58cabbeba90a0425f22dd108b481dd188d2507
Kabir Chandra <kabirchandra@Kabirs-MacBook-Pro.local>
2024-06-07 04:51:47 +1000

    update

--- VM.sh
+++ VM.sh
@@ -1,8 +1,8 @@
 
 
 clear
-gcc ./src/compiler_structs.c ./src/intepret_IR.c ./src/main.c ./src/stack.c ./src/storage_controller.c -o ./output/out
-./output/out
+gcc ./src/compiler_structs.c ./src/intepret_IR.c ./src/main.c ./src/stack.c ./src/storage_controller.c -o ./output/VM_OUT
+./output/VM_OUT
 
 
 
